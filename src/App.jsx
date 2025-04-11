82100bf76ba29d277e53dc6033742b7714433835
EnricoCiccolini <piroroma90@gmail.com>
2025-04-11 14:23:17 +0200

    feat: second milestone

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,28 @@ function App() {
   return (
     <>
       <main>
-        <h1>hi</h1>
+        <div>
+          {actres.map(item => (
+            <div key={item.id}>
+              <div className="card" style={{ width: 18 + 'rem ' }}>
+                <img src={item.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{item.name}</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">birth year {item.birth_year}</li>
+                  <li className="list-group-item">naziolity {item.nationality}</li>
+                  <li className="list-group-item">biography {item.biography}</li>
+                  <li className="list-group-item">awards {item.awards}</li>
+                </ul>
+
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div></div>
       </main>
     </>
   )
