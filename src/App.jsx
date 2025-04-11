9af81abc72a29f133befd3ae51c38443926b2ed1
EnricoCiccolini <piroroma90@gmail.com>
2025-04-11 15:27:12 +0200

    feat: bonus 2

--- src/App.jsx
+++ src/App.jsx
@@ -22,20 +22,22 @@ function App() {
 
 
   useEffect(actresapi, [])
-
-
   useEffect(actorsapi, [])
-  console.log(actres)
-  console.log(actor)
+
+
+  const actorsAndActres = [...actor, ...actres]
+
+
+
 
   return (
     <>
       <main>
         <div className="container text-center">
-          <h2>ACCTRES</h2>
+          <h2>ACCTRES and ACTORS</h2>
           <div className="d-flex flex-wrap gap-3">
-            {actres.map(item => (
-              <div key={item.id}>
+            {actorsAndActres.map(item => (
+              <div key={item.id + item.name}>
                 <div className="card col-4" style={{ width: 18 + 'rem ' }}>
                   <img src={item.image} className="card-img-top" alt="..." />
                   <div className="card-body">
@@ -54,28 +56,7 @@ function App() {
 
             ))}
 
-          </div>
-          <h2>ACTORS</h2>
-          <div className="d-flex flex-wrap gap-3">
-            {actor.map(item => (
-              <div key={item.id}>
-                <div className="card col-4" style={{ width: 18 + 'rem ' }}>
-                  <img src={item.image} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{item.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">birth year {item.birth_year}</li>
-                    <li className="list-group-item">naziolity {item.nationality}</li>
-                    <li className="list-group-item">biography {item.biography}</li>
-                    <li className="list-group-item">awards {item.awards}</li>
-                  </ul>
 
-                </div>
-              </div>
-
-            ))}
 
           </div>
 
